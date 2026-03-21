fa69756d79c3dac2fb91f8c2827280d71d71fdbc
Banti Kumar Singh <bantikumarsingh91@gmail.com>
2026-03-21 12:37:47 +0530

    change again

--- src/lib/redux/slices/chatSlice.js
+++ src/lib/redux/slices/chatSlice.js
@@ -86,6 +86,7 @@ const chatSlice = createSlice({
       state.messages = action.payload;
     },
     
+    
     addMessage: (state, action) => {
       const exists = state.messages.find(m => m._id === action.payload._id || m.id === action.payload._id);
       if (!exists) {
